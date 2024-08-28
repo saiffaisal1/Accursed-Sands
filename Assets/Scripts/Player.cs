255420e166343e7c0936d1928d880e20e300b8e4
MysticZafar <skhzafar110@gmail.com>
2024-08-28 20:59:16 +0400

    added Health Bar and Health system
    adding health system with a health bar

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,15 +7,66 @@ public class Player : MonoBehaviour
     public int maxHealth = 100;
     int currentHealth = 0;
 
+    public HealthBar healthbar;
+
+    public Animator animator;
+ 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        currentHealth = maxHealth;
+        healthbar.setMaxHealth(maxHealth);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            takeDamage(10);
+            animator.SetTrigger("Hurt");
+
+
+        }
     }
+
+    void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthbar.setHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+
+        }
+    }
+
+    void Die()
+    {
+        animator.SetBool("isDead", true);
+
+
+        this.enabled = false;
+        cantMove();
+        cantAttack();
+    }
+
+    void cantMove()
+    {
+        GetComponent<PlayerMovement>().enabled = false;
+    }
+
+    void cantAttack()
+    {
+        GetComponent<PlayerCombat>().enabled = false;
+    }
+
 }
